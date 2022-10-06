ba0c8112141880b11d727115666a2ae873bc4c2b
Keval <keval.sayar@globalvoxinc.com>
2022-10-06 10:46:48 +0530

    added timeleft and shortened benef address shown

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import blindAbi from "./contractabi/blindabi.json" assert { type: "json" };
 const web3 = new Web3(window.ethereum);
 var MyContract = new web3.eth.Contract(
   blindAbi,
-  "0x73949b013deD3568cf33ff359C3cEbcD260EBA3e"
+  "0x0d68E2d9B35dca95d1F30c15BdB65643Ce34721e"
 );
 
 function sayHi() {
@@ -10,7 +10,10 @@ function sayHi() {
     .beneficiary()
     .call()
     .then(function (result) {
-      document.getElementById("beneficiary").innerText = result;
+      const addressStart = result.substring(0, 5),
+       addressEnd = result.substring(result.length - 4),
+       newAddress = addressStart + "..." + addressEnd;
+      document.getElementById("beneficiary").innerText = newAddress;
     });
   MyContract.methods
     .highestBidder()
@@ -27,6 +30,29 @@ function sayHi() {
       if (result !== "0") {
         document.getElementById("highestBid").innerText = result;
       }
+    });
+  MyContract.methods
+    .biddingEnd()
+    .call()
+    .then(function (result) {
+      var countDownDate = new Date(result * 1000).getTime();
+      var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("timeleft").innerHTML =
+          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "Auction Completed!";
+        }
+      }, 1000);
+      console.log(new Date(result * 1000));
     });
 }
 
