0f6dd0651d286a258551afaad29c0042f09ab4b6
Keval <keval.sayar@globalvoxinc.com>
2022-10-06 18:35:30 +0530

    ether bug solved

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import blindAbi from "./contractabi/blindabi.json" assert { type: "json" };
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner()
-const MyContract = new ethers.Contract(
-  "0x547AE3374aa31218976BdD624305905269FC5962",
-  blindAbi,
-  signer
-);
 
 const shortenString = (input) => {
   const start = input.substring(0, 5),
@@ -15,14 +9,22 @@ const shortenString = (input) => {
 };
 
 const transact = () => {
-  const bidValue = document.getElementById('bidValue').value.toString();
-  const secretKey = document.getElementById('secretKey').value;
-  MyContract.blind_a_bid(bidValue, secretKey).then(async function(result){
-    MyContract.bid(result, {value: ethers.utils.parseEther(bidValue)}).then(function(result){
-      console.log(result);
-    })
-  })
-}
+  const signer = provider.getSigner();
+  const MyContract = new ethers.Contract(
+    "0x547AE3374aa31218976BdD624305905269FC5962",
+    blindAbi,
+    signer
+  );
+  const bidValue = document.getElementById("bidValue").value.toString();
+  const secretKey = document.getElementById("secretKey").value;
+  MyContract.blind_a_bid(bidValue, secretKey).then(async function (result) {
+    MyContract.bid(result, { value: ethers.utils.parseEther(bidValue) }).then(
+      function (result) {
+        console.log(result);
+      }
+    );
+  });
+};
 
 async function connectToMetamask() {
   try {
@@ -39,8 +41,20 @@ async function connectToMetamask() {
 async function initializeData() {
   await provider.listAccounts().then(async function (accounts) {
     if (accounts.length == 0) {
-      document.getElementById("connectToMetamask").style.display = "block";
+      document.getElementById("connectDiv").style.display = "flex";
     } else {
+      const signer = provider.getSigner();
+      const MyContract = new ethers.Contract(
+        "0x547AE3374aa31218976BdD624305905269FC5962",
+        blindAbi,
+        signer
+      );
+      MyContract.on("AuctionEnded", function (highestBidder, highestBid) {
+        document.getElementById("showHighestBidder").innerText =
+          shortenString(highestBidder);
+        document.getElementById("showHighestBid").innerText =
+          ethers.utils.formatEther(highestBid) + " TBNB";
+      });
       document.getElementById("connectToMetamask").style.display = "none";
       document.getElementById("accountAddress").innerText = shortenString(
         accounts[0]
@@ -49,70 +63,79 @@ async function initializeData() {
         ethers.utils
           .formatEther(await provider.getBalance(accounts[0]))
           .substring(0, 7) + " TBNB";
+      MyContract.biddingEnd().then(function (result) {
+        var countDownDate = new Date(result * 1000).getTime();
+        var x = setInterval(function () {
+          var now = new Date().getTime();
+          var distance = countDownDate - now;
+          if (distance <= 0) {
+            clearInterval(x);
+            document.getElementById("timeleftforbidding").innerHTML =
+              "Bidding Done!";
+            MyContract.revealEnd().then(function (result) {
+              document.getElementById("bidAndSecretKey").style.display = "none";
+              document.getElementById("deposit").style.display = "none";
+              document.getElementById("highestBidder").style.display = "block";
+              document.getElementById("highestBid").style.display = "block";
+              MyContract.highestBid().then(function (result) {
+                if (result !== "0") {
+                  document.getElementById("showHighestBid").innerText =
+                    ethers.utils.formatEther(result) + " TBNB";
+                }
+              });
+              MyContract.highestBidder().then(function (result) {
+                if (result !== "0x0000000000000000000000000000000000000000") {
+                  document.getElementById("showHighestBidder").innerText =
+                    shortenString(result);
+                }
+              });
+              var countDownDate = new Date(result * 1000).getTime();
+              var x = setInterval(function () {
+                var now = new Date().getTime();
+                var distance = countDownDate - now;
+                if (distance < 0) {
+                  clearInterval(x);
+                  document.getElementById("timeleftforreveal").innerHTML =
+                    "Reveal Done!";
+                } else {
+                  document.getElementById("revealDiv").style.display = "flex";
+                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                  var hours = Math.floor(
+                    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+                  );
+                  var minutes = Math.floor(
+                    (distance % (1000 * 60 * 60)) / (1000 * 60)
+                  );
+                  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                  document.getElementById("timeleftforreveal").innerHTML =
+                    days +
+                    "d " +
+                    hours +
+                    "h " +
+                    minutes +
+                    "m " +
+                    seconds +
+                    "s ";
+                }
+              }, 1000);
+            });
+          } else {
+            document.getElementById("bidAndSecretKey").style.display = "flex";
+            document.getElementById("deposit").style.display = "flex";
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor(
+              (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            var minutes = Math.floor(
+              (distance % (1000 * 60 * 60)) / (1000 * 60)
+            );
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            document.getElementById("timeleftforbidding").innerHTML =
+              days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+          }
+        }, 1000);
+      });
     }
-  });
-  MyContract.biddingEnd().then(function (result) {
-    var countDownDate = new Date(result * 1000).getTime();
-    var x = setInterval(function () {
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-      if (distance <= 0) {
-        clearInterval(x);
-        document.getElementById("timeleftforbidding").innerHTML =
-          "Bidding Done!";
-        MyContract.revealEnd().then(function (result) {
-          document.getElementById("bidAndSecretKey").style.display = "none";
-          document.getElementById("deposit").style.display = "none";
-          document.getElementById("highestBidder").style.display = "block";
-          document.getElementById("highestBid").style.display = "block";
-          MyContract.highestBid().then(function (result) {
-            if (result !== "0") {
-              document.getElementById("showHighestBid").innerText =
-                ethers.utils.formatEther(result) + " TBNB";
-            }
-          });
-          MyContract.highestBidder().then(function (result) {
-            if (result !== "0x0000000000000000000000000000000000000000") {
-              document.getElementById("showHighestBidder").innerText =
-                shortenString(result);
-            }
-          });
-          var countDownDate = new Date(result * 1000).getTime();
-          var x = setInterval(function () {
-            var now = new Date().getTime();
-            var distance = countDownDate - now;
-            if (distance < 0) {
-              clearInterval(x);
-              document.getElementById("timeleftforreveal").innerHTML =
-                "Reveal Done!";
-            } else {
-              document.getElementById("revealDiv").style.display = "flex";
-              var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-              var hours = Math.floor(
-                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-              );
-              var minutes = Math.floor(
-                (distance % (1000 * 60 * 60)) / (1000 * 60)
-              );
-              var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-              document.getElementById("timeleftforreveal").innerHTML =
-                days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-            }
-          }, 1000);
-        });
-      } else {
-        document.getElementById("bidAndSecretKey").style.display = "flex";
-        document.getElementById("deposit").style.display = "flex";
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("timeleftforbidding").innerHTML =
-          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-      }
-    }, 1000);
   });
 }
 document.addEventListener("DOMContentLoaded", initializeData);
@@ -120,11 +143,6 @@ document
   .getElementById("connectToMetamask")
   .addEventListener("click", connectToMetamask);
 
-document.getElementById('transact').addEventListener('click', transact)
+document.getElementById("transact").addEventListener("click", transact);
 
-MyContract.on("AuctionEnded", function (highestBidder, highestBid) {
-  document.getElementById("showHighestBidder").innerText =
-    shortenString(highestBidder);
-  document.getElementById("showHighestBid").innerText =
-    ethers.utils.formatEther(highestBid) + " TBNB";
-});
+
